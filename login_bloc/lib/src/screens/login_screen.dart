93def75bafd689c2c00ba2082d5d7fd9e04fb6f3
Rajesh Vj <rajeshvj.2412@gmail.com>
2022-09-02 11:22:03 +0530

    Login example with Bloc with Global and Scoped Instance and with rxDart streams.

--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../blocs/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) {
+    final bloc = Provider.of(context);
+
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
         child: Column(
           children: [
-            emailField(),
-            passwordField(),
+            emailField(bloc),
+            passwordField(bloc),
             Container(margin: EdgeInsets.only(top: 25.0)),
-            submitButton(),
+            submitButton(bloc),
           ],
         ),
       ),
     );
   }
 
-  Widget emailField() {
+  Widget emailField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.email,
       builder: ((context, snapshot) {
@@ -35,7 +38,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget passwordField() {
+  Widget passwordField(Bloc bloc) {
     return StreamBuilder(
       stream: bloc.password,
       builder: ((context, snapshot) {
@@ -51,11 +54,16 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      color: Colors.blue,
-      child: Text('Submit!'),
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: snapshot.hasData ? bloc.submit : null,
+        );
+      },
     );
   }
 }
